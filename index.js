3096bc3cb0f1f4097f7ac33d45fcc132cf1b2482
Bhagyasri117 <138472013+Bhagyasri117@users.noreply.github.com>
2023-09-23 12:33:09 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,75 +1,43 @@
 const express = require('express');
-const { status } = require('express/lib/response');
+
 const app = express();
-const port = 3000;
+const port = 2000;
 app.use(express.json());
-app.get('/trains',async(req,res)=> {
-    try{
-        const trainData = [
-            {
-                trainNumber: '12267',
-                trainname: "MUMBAI CENTRAL - AHMEDABAD AC Duronto Exp",
-                trainType: "Duranto",
-                runDays: "M,T,W,T,F,S,S",
-                departureTime: '23:25',
-                seatsSleeper: Math.floor(Math.random() * 151)+50,
-                priceSleeper: Math.floor(Math.random() * 1501)+500,
-                seatsAC: Math.floor(Math.random() * 151)+50,
-                priceAC: Math.floor(Math.random() * 1501)+500,
-            },
-            {
-                trainNumber: '12268',
-                trainname: " AHMEDABAD - MUMBAI CENT AC Duronto Exp",
-                trainType: "Duranto",
-                runDays: "M,T,W,T,F,S,S",
-                departureTime: '23:40',
-                seatsSleeper: Math.floor(Math.random() * 151)+50,
-                priceSleeper: Math.floor(Math.random() * 1501)+500,
-                seatsAC: Math.floor(Math.random() * 151)+50,
-                priceAC: Math.floor(Math.random() * 1501)+500,
+app.get('/numbers', async (req, res) => {
+    const urls = req.query.url;
+if (!urls) {
 
+return res.status(400).json({ error: 'No URLs provided'});
+}
+const urlArray=Array.isArray(urls) ? urls : [urls];
+async function fetchAndMergeNumbers(url) { 
+    try {
 
-            },
-            {
-                trainNumber: '2201',
-                trainname: "KOLKATA SEALDAH -PURI Duronto Express",
-                trainType: "Duranto",
-                runDays: "M,W,F",
-                departureTime: '20:00',
-                seatsSleeper: Math.floor(Math.random() * 151)+50,
-                priceSleeper: Math.floor(Math.random() * 1501)+500,
-                seatsAC: Math.floor(Math.random() * 151)+50,
-                priceAC: Math.floor(Math.random() * 1501)+500,
-            },
-        ];
-        const currentTime = new Date();
-        const  twelveHoursLater = new Date(currentTime.getTime() + 12 * 60 * 60 *1000);
-        const filteredTrains = trainData.filter(train => {
-            const departureTime = new Date(train.departureTime);
-            return (
-                currentTime.getTime() + 30 * 60 * 1000 < departureTime.getTime() &&
-                departureTime.getTime() <= tweleveHoursLater.getTime()
-            );
-        });
-        const sortedTrains = filteredTrains.sort((a,b) => {
-            if (a.pricesSleeper !== b.priceSleeper) {
-                return a.priceSleeper - b.priceSleeper;
+        const response = await axios.get(url, {timeout: 500 });
 
-            } else if (a.seatsSleeper !== b.seatsSleeper) {
-                return b.seatsSleeper - a.seatsSleeper;
-            } else {
-                const aDepartureTime = new Date(a.departureTime).getHours();
-                const bDepartureTime = new Date(b.departureTime).getHours();
-                return bDepartureTime - aDepartureTime;
-            }
-        });
-        res.json(sortedTrains);
-    } catch (error){
-        console.error('Error:',error);
-        res,status(500).json({error: 'Internal server error'});
+        if (response.status === 200 && Array.isArray(response.data.numbers)) {
+
+            return response.data.numbers;
+        }
+        return [];
+
+    } catch (error) {
+      return [];
     }
+}
 
+const mergedNumbers = [];
+const promises = urlArray.map(async (url) => {
+    const numbers = await fetchAndMergeNumbers(url);
+    mergedNumbers.push(numbers);
 });
+
+await Promise,all(promises);
+const uniqueSortedNumbers = [...new Set (mergedNumbers)].sort((a, b) => a - b);
+res.json({ numbers: uniqueSortedNumbers });
+});
+
 app.listen(port, () => {
-    console.log('Server is listening at http://localhost:${port}');
+ console.log('Server is listening at http://localhost:${port}');
 });
+
